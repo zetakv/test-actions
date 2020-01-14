f604cf147ab759f7d2f955d32d17e76b21cfb451
zetakv <61610092@qq.com>
2020-01-14 01:40:34 +0000

    Deploying to gh-pages from master 05ac9679e2ec14a9dfa7ac7d2fd8503fe090bcfb

---
+++ precache-manifest.ff0b99e75646b80bf3c2212b72b670a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9530fd96df1ad5a11dac7840ea2404d",
+    "url": "/test-actions/index.html"
+  },
+  {
+    "revision": "03ee338b0fc413fb78c5",
+    "url": "/test-actions/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33f35c1fe12e2ba02718",
+    "url": "/test-actions/static/js/2.84d5110c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-actions/static/js/2.84d5110c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03ee338b0fc413fb78c5",
+    "url": "/test-actions/static/js/main.ecd5e874.chunk.js"
+  },
+  {
+    "revision": "f24ff5b2df7e8349d994",
+    "url": "/test-actions/static/js/runtime-main.3983b84b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-actions/static/media/logo.5d5d9eef.svg"
+  }
+]);
